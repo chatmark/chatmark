88ca889aae1b6c6cf57628351a8afa4ca48416c5
Chris Chan <chrischan@igpsd.com>
2019-03-08 00:39:13 +0800

    update serverless to handle verify

--- handler.js
+++ handler.js
@@ -1,5 +1,45 @@
 'use strict';
 
+module.exports.callback = async (event, context) => {
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "123456"
+  let query = event.queryStringParameters
+  // Parse the query params
+  let mode = query['hub.mode'];
+  let token = query['hub.verify_token'];
+  let challenge = query['hub.challenge'];
+    
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+  
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      // res.status(200).send(challenge);
+      return {
+        statusCode: 200,
+        body: challenge,
+      };
+    
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
 module.exports.hello = async (event, context) => {
   return {
     statusCode: 200,
